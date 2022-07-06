79d263de7b2bc0410a224d1e25df97d5aac655fe
NicoF85 <nfunes@vairix.com>
2022-07-06 15:34:30 -0300

    add initial code

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+Rails.application.routes.draw do
+  resources :portfolios
+  get 'pages/home'
+
+  get 'pages/about'
+
+  get 'pages/contact'
+
+  resources :blogs
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
