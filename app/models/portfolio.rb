eb6f76b911c99a3445d4e61eb246a09829cc33d9
NicoF85 <nfunes@vairix.com>
2022-07-07 14:57:47 -0300

    add portfolio defaults values and add missing value in form

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 class Portfolio < ApplicationRecord
+  validates :title, :body, :main_image, :thumb_image, presence: true
+  scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.main_image ||= 'https://via.placeholder.com/600x300'
+    self.thumb_image ||= 'https://via.placeholder.com/150x75'
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,30 +1,47 @@
-10.times do |blog|
-    Blog.create!(
-        title: "My blog Post #{blog}",
-        body: "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains."
-    )
+3.times do |topic|
+    Topic.create!(title: "Topic #{topic}")
 end
 
-puts "10 blog posts created"
+puts '3 topics created'
+
+10.times do |blog|
+  Blog.create!(
+    title: "My blog Post #{blog}",
+    body: 'On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains.',
+    topic_id: Topic.last
+  )
+end
+
+puts '10 blog posts created'
 
 5.times do |skills|
-    Skill.create!(
-        title: "Rails #{skills}",
-        percent_utilized: 15
-    )
+  Skill.create!(
+    title: "Rails #{skills}",
+    percent_utilized: 15
+  )
 end
 
-puts "5 skills created"
+puts '5 skills created'
 
 9.times do |portfolio_item|
-    Portfolio.create!(
-        title: "Portfolio title #{portfolio_item}",
-        subtitle: "My great service",
-        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        main_image: "https://via.placeholder.com/600x300",
-        thumb_image: "https://via.placeholder.com/150x75"
-
-    )
+  Portfolio.create!(
+    title: "Portfolio title: #{portfolio_item}",
+    subtitle: 'Ruby on Rails',
+    body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    main_image: 'https://via.placeholder.com/600x300',
+    thumb_image: 'https://via.placeholder.com/150x75'
+  )
 end
 
-puts "9 Portfolio items created"
+1.times do |portfolio_item|
+    Portfolio.create!(
+      title: "Portfolio title: #{portfolio_item}",
+      subtitle: 'Angular',
+      body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      main_image: 'https://via.placeholder.com/600x300',
+      thumb_image: 'https://via.placeholder.com/150x75'
+    )
+  end
+  
+
+puts '10 Portfolio items created'
