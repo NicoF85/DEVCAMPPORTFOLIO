eaa99bbba84e5ab1f6bfe4c6852829d603678353
NicoF85 <nfunes@vairix.com>
2022-07-13 16:47:35 -0300

    drag and drop functionality

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -6,6 +6,7 @@ class Portfolio < ApplicationRecord
   accepts_nested_attributes_for :technologies, reject_if: ->(attrs) { attrs['name'].blank? }
   validates :title, :body, :main_image, :thumb_image, presence: true
   scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }
+  scope :by_position, -> {     order('position ASC')  }
   after_initialize :set_defaults
 
   def set_defaults
