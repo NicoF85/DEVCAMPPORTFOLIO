ed4f837ed724f28a9ed6c843ca1ed0e8b47fdb37
NicoF85 <108738896+NicoF85@users.noreply.github.com>
2022-07-14 13:42:16 -0300

    T10- Section 16 Project Images (#10)
    * add images to the project and uploader
    
    * add images to the project and uploader

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,16 +1,12 @@
 # frozen_string_literal: true
 
 class Portfolio < ApplicationRecord
-  include Placeholder
   has_many :technologies, dependent: nil
   accepts_nested_attributes_for :technologies, reject_if: ->(attrs) { attrs['name'].blank? }
-  validates :title, :body, :main_image, :thumb_image, presence: true
+  validates :title, :body, presence: true
   scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }
-  scope :by_position, -> {     order('position ASC')  }
-  after_initialize :set_defaults
+  scope :by_position, -> {     order('position ASC') }
 
-  def set_defaults
-    self.main_image ||= Placeholder.image_generator(height: 600, width: 300)
-    self.thumb_image ||= Placeholder.image_generator(height: 150, width: 75)
-  end
+  mount_uploader :thumb_image, PortfolioUploader
+  mount_uploader :main_image, PortfolioUploader
 end
